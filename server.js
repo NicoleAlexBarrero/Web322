e99727c8cbe12d0e9169d3e1fc2bc1c27a2d1d84
NicoleAlexBarrero <113063376+NicoleAlexBarrero@users.noreply.github.com>
2023-01-17 15:43:41 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//running a web server
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Nicole Barrero - 158270215");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
